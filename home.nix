979875ee7fba46a9864006591f5caf663a6349a4
Christopher Grim <christopher.grim@gmail.com>
2021-12-27 15:56:46 -0500

    Bootstrap a bare bones home environment

---
+++ home.nix
@@ -0,0 +1,77 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "grim";
+  home.homeDirectory = "/Users/grim";
+
+  home.stateVersion = "22.05";
+
+  home.packages = [
+    pkgs.bottom
+    pkgs.fd
+    pkgs.ripgrep
+  ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.bat = {
+    enable = true;
+    config = {
+      theme = "gruvbox-dark";
+      style = "plain";
+    };
+  };
+
+  programs.exa = {
+    enable = true;
+    enableAliases = true;
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Christopher Grim";
+    userEmail = "christopher.grim@gmail.com";
+    aliases = {
+      ci = "commit";
+      co = "checkout";
+      diffc = "diff --cached";
+      st = "status --short";
+    };
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
+    shellAliases = {
+      "g" = "git";
+    };
+    sessionVariables = {
+      EDITOR = "nvim";
+    };
+  };
+
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      fugitive
+      gruvbox
+      vim-nix
+    ];
+
+    extraConfig = ''
+      set nocompatible
+      colorscheme gruvbox
+      set relativenumber
+      set number
+    '';
+  };
+}
