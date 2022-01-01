205d8179a8da6f4f14162bd2b6aa4919d42bfa90
Christopher Grim <christopher.grim@gmail.com>
2022-01-01 12:45:50 -0500

    Add common neovim settings
    Just a bunch of settings ported over from my old config.

--- modules/neovim/config/lua/_init.lua
+++ modules/neovim/config/lua/_init.lua
@@ -1,7 +1,25 @@
 -- home-manager generates an init.vim, so we source this file on startup
 
+vim.g.mapleader = ' '
+
+vim.g.autoindent = true
+vim.g.expandtab = true
+vim.g.hidden = true
+vim.g.signcolumn = true
+vim.g.smartindent = true
+vim.g.spell = true
+vim.g.spelllang = "en_us"
+vim.g.termguicolors = true
+vim.g.undodir = vim.fn.expand("~/.config/nvim/undo")
+vim.g.undofile = true
+vim.g.updatetime = 500
+
+vim.o.expandtab = true
 vim.o.scrolloff = 7
+
 vim.wo.number = true
 vim.wo.relativenumber = true
 
-vim.cmd 'colorscheme gruvbox'
+vim.cmd [[colorscheme gruvbox]]
+vim.cmd [[syntax enable]]
+vim.cmd [[filetype plugin indent on]]
