20b8c06c41d85b1581622c78bb2807cfef5780c0
Christopher Grim <christopher.grim@gmail.com>
2022-01-16 17:49:41 -0500

    Move git config to its own module

--- home.nix
+++ home.nix
@@ -3,10 +3,12 @@
 {
   home.username = "grim";
   home.homeDirectory = "/Users/grim";
-
   home.stateVersion = "22.05";
 
+  programs.home-manager.enable = true;
+
   imports = [
+    ./modules/git
     ./modules/neovim
     ./modules/shell
     ./modules/tmux
@@ -15,23 +17,4 @@
   home.packages = [
     pkgs.lorri
   ];
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  programs.git = {
-    enable = true;
-    userName = "Christopher Grim";
-    userEmail = "christopher.grim@gmail.com";
-    aliases = {
-      ci = "commit";
-      co = "checkout";
-      diffc = "diff --cached";
-      st = "status --short";
-    };
-    extraConfig = {
-      init.defaultBranch = "main";
-    };
-  };
-
 }
---
+++ modules/git/default.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+  programs.git = {
+    enable = true;
+    userName = "Christopher Grim";
+    userEmail = "christopher.grim@gmail.com";
+    aliases = {
+      ci = "commit";
+      co = "checkout";
+      diffc = "diff --cached";
+      st = "status --short";
+    };
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
+}
