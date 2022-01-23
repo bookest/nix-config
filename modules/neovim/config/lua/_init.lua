330601766505376dc7e44e6fe36c739fe366d06b
Christopher Grim <christopher.grim@gmail.com>
2022-01-23 16:42:36 -0500

    Wait 2 seconds before showing blame virtual text
    I found the blame virtual text to be popping up too often as I moved
    around a file, so bumping up the delay should help reduce how frequently
    it shows up.

--- modules/neovim/config/lua/_init.lua
+++ modules/neovim/config/lua/_init.lua
@@ -41,7 +41,7 @@ require('gitsigns').setup {
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = 'right_align',
-    delay = 1000,
+    delay = 2000,
     ignore_whitespace = true,
   },
 }
