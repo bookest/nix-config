09285e7755bb92abe83945a559c33b0290feefef
Christopher Grim <christopher.grim@gmail.com>
2022-01-23 16:38:03 -0500

    Give starship a try
    I'm not sure this is saving me any work, as I had to customize starship
    a ton, but it does handle some nice git things that I normally would
    have done via gnarly shell functions and I don't mind not writing that
    code.

--- modules/shell/default.nix
+++ modules/shell/default.nix
@@ -22,6 +22,58 @@
     enableAliases = true;
   };
 
+  programs.starship = {
+    enable = true;
+    settings = {
+      format = pkgs.lib.concatStrings [
+        "$time " 
+        "<"
+        "$username"
+        "@"
+        "$hostname"
+        ":"
+        "$directory"
+        "> "
+        "$git_branch"
+        "$git_state"
+        "$fill"
+        "$cmd_duration"
+        "$line_break"
+        "$character"
+      ];
+      character = {
+        success_symbol = ''[\$](bold green)'';
+        error_symbol = ''[\$](bold red)'';
+      };
+      cmd_duration = {
+        format = ''[\[$duration\]]($style)'';
+      };
+      directory = {
+        truncate_to_repo = false;
+        format = ''[$path]($style)'';
+      };
+      fill = { 
+        symbol = " ";
+      };
+      git_branch = {
+        format = ''[\($branch\)]($style)'';
+      };
+      time = {
+        disabled = false;
+        format = ''[\[$time\]]($style)'';
+        style = "blue"; 
+      };
+      username = {
+        format = ''[$user]($style)'';
+        show_always = true;
+      };
+      hostname = {
+        ssh_only = false;
+        format = ''[$hostname]($style)'';
+      };
+    };
+  };
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
