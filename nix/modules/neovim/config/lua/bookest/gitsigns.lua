b6286bd94d49763828b0630d37b6255dc8448652
Christopher Grim <christopher.grim@gmail.com>
2022-04-30 16:45:19 -0400

    Setup gitsigns keybindings

--- nix/modules/neovim/config/lua/bookest/gitsigns.lua
+++ nix/modules/neovim/config/lua/bookest/gitsigns.lua
@@ -39,4 +39,20 @@ require('gitsigns').setup {
     delay = 2000,
     ignore_whitespace = true,
   },
+  on_attach = function(bufnr)
+    local gitsigns = package.loaded.gitsigns
+    local opts = { buffer = bufnr }
+
+    vim.keymap.set('n', ']c', gitsigns.next_hunk, opts)
+    vim.keymap.set('n', '[c', gitsigns.prev_hunk, opts)
+    vim.keymap.set({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>', opts)
+    vim.keymap.set({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>', opts)
+    vim.keymap.set('n', '<leader>hS', gitsigns.stage_buffer, opts)
+    vim.keymap.set('n', '<leader>hu', gitsigns.undo_stage_hunk, opts)
+    vim.keymap.set('n', '<leader>hR', gitsigns.reset_buffer, opts)
+    vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk, opts)
+
+    -- Hunk text object
+    vim.keymap.set({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end,
 }
