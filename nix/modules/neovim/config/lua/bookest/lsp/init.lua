3ea07c09bb12802e1c0c306236e675c7e5213f38
Christopher Grim <christopher.grim@gmail.com>
2022-11-24 13:39:13 -0500

    fix(nvim): fix breaking change in cmp=nvim=lsp
    See https://github.com/hrsh7th/cmp-nvim-lsp/issues/38

--- nix/modules/neovim/config/lua/bookest/lsp/init.lua
+++ nix/modules/neovim/config/lua/bookest/lsp/init.lua
@@ -11,7 +11,7 @@ local on_attach = function(_, bufnr)
   vim.keymap.set('n', '<leader>fs', require('telescope.builtin').lsp_document_symbols, opts)
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local servers = {
   'bashls',
