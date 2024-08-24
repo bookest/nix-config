c582a03167fd8feb9f32f032a9426c30ffa471da
Christopher Grim <christopher.grim@gmail.com>
2024-08-24 13:19:57 -0400

    feat(wezterm): Add binding for Vimium style URL opening

--- nix/modules/wezterm/config/wezterm.lua
+++ nix/modules/wezterm/config/wezterm.lua
@@ -14,4 +14,19 @@ config.font_size = 15.0
 
 config.audible_bell = 'Disabled'
 
+config.keys = {
+  {
+    key = 'F',
+    mods = 'SHIFT|CTRL',
+    action = wezterm.action.QuickSelectArgs {
+      patterns = { 'https?://\\S+' },
+      action = wezterm.action_callback(function(window, pane)
+        local url = window:get_selection_text_for_pane(pane)
+        wezterm.log_info('opening: ' .. url)
+        wezterm.open_with(url)
+      end),
+    },
+  },
+}
+
 return config
